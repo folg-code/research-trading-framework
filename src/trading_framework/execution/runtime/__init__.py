0e95f394225d35712184fb6607f0b2505146e343
Filip Folga <folga33@gmail.com>
2026-07-15 22:23:26 +0200

    Add local execution runtime session

--- src/trading_framework/execution/runtime/__init__.py
+++ src/trading_framework/execution/runtime/__init__.py
@@ -1,5 +1,6 @@
 """Execution runtime orchestration helpers."""
 
+from trading_framework.execution.runtime.session import LocalExecutionRuntimeSession
 from trading_framework.execution.runtime.strategy_orders import (
     StrategyModelOrderAdapter,
     StrategyOrderDecision,
@@ -7,6 +8,7 @@ from trading_framework.execution.runtime.strategy_orders import (
 )
 
 __all__ = [
+    "LocalExecutionRuntimeSession",
     "StrategyModelOrderAdapter",
     "StrategyOrderDecision",
     "StrategyOrderDecisionType",
